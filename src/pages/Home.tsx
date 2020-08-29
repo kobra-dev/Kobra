f43fa8b5e8f6edc9ae05e6d1a1d9e4e1de6ff3b0
Dragon He <45088170+abstractultra@users.noreply.github.com>
2020-08-29 13:14:11 -0400

    Cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,14 +6,14 @@ import PageLayout from "../components/PageLayout";
 const toolbox: string = `
 <xml id="toolbox" style="display: none">
 	<category name="Control" colour="230">
-		<block type="controls_if"></block>
-		<block type="controls_whileUntil"></block>
-		<block type="controls_for"></block>
+		<block type="controls_if"/>
+		<block type="controls_whileUntil"/>
+		<block type="controls_for"/>
 	</category>
 	<category name="Logic" colour="210">
-		<block type="logic_compare"></block>
-		<block type="logic_operation"></block>
-		<block type="logic_boolean"></block>
+		<block type="logic_compare"/>
+		<block type="logic_operation"/>
+		<block type="logic_boolean"/>
 	</category>
 </xml>
 `;
@@ -26,7 +26,7 @@ export default function Home(): React.ReactElement {
   }, []);
   return (
     <PageLayout title="Kobra Studio">
-      <div id="codeEditor" style={{ height: "90vh", width: "100%" }}></div>
+      <div id="codeEditor" style={{height: "90vh", width: "100%"}}/>
     </PageLayout>
   );
 };
