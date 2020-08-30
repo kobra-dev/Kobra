9b96ccef2488845c94a28c1b0b4a520e776769f4
Dragon He <45088170+abstractultra@users.noreply.github.com>
2020-08-30 02:04:30 -0400

    Grid works

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import "./Home.css";
-import PageLayout from "../components/PageLayout";
+import React from 'react';
+import './Home.css';
+import PageLayout from '../components/PageLayout';
 import CodeEditor from '../components/CodeEditor';
+import { Grid, Paper, Typography } from '@material-ui/core';
 
 export default function Home(): React.ReactElement {
-  return (
-    <PageLayout title="Kobra Studio">
-			<CodeEditor />
-    </PageLayout>
-  );
+	return (
+		<PageLayout title="Kobra Studio">
+			<div className="gridContainer">
+				<Paper style={{ gridArea: 'editor' }}>
+					<CodeEditor />
+				</Paper>
+				<Paper style={{gridArea: 'panel1'}}>
+					<Typography>Hi</Typography>
+				</Paper>
+				<Paper style={{gridArea: 'panel2'}}>
+					<Typography>Hi</Typography>
+				</Paper>
+			</div>
+		</PageLayout>
+	);
 }
