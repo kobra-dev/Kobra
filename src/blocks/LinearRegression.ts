53965a24be3cfff7e058bf6d338d0abd8b92310c
Pranav Teegavarapu <pranavnt@outlook.com>
2020-08-30 03:31:57 -0700

    basic progress on appendProgress complete ig

--- src/blocks/LinearRegression.ts
+++ src/blocks/LinearRegression.ts
@@ -14,11 +14,20 @@ function multivariateLinearRegression(X: [Float32Array], y: [Float32Array]) {
 	return;
 }
 
-function appendProgress(
+function appendProgressUniLinReg(
 	progress: number,
 	slope: number,
 	intercept: number,
+	mse: number,
 	r2: number
 ) {
+	var newElem: HTMLElement = document.createElement('div');
+	newElem.innerHTML = '';
+	return;
+}
+
+function appendProgressMultiLinReg(progress: number, mse: number, r2: number) {
+	var newElem: HTMLElement = document.createElement('div');
+	newElem.innerHTML = '';
 	return;
 }
