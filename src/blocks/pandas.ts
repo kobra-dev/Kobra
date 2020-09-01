44e4cefd1e3de81f3da51156f3cd6a4ac5d3e12d
Pranav Teegavarapu <pranavnt@outlook.com>
2020-08-31 21:59:06 -0700

    renamed files & created read_csv function

--- src/blocks/pandas.ts
+++
@@ -1,3 +0,0 @@
-var regressionJS = document.createElement('script');
-regressionJS.src = '../../node_modules/data-forge/dist/web/index.js';
-document.head.appendChild(regressionJS);
---
+++ src/blocks/read_csv.ts
@@ -0,0 +1,5 @@
+let regressionJS: HTMLElement = document.createElement('script');
+regressionJS.src = '../../node_modules/data-forge/dist/web/index.js';
+document.head.appendChild(regressionJS);
+
+export default function read_csv(csv_path: string) {}
