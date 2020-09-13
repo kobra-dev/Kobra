25e940b8ceb36777770487f0ab1edf4798b33a23
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-12 23:41:57 -0700

    working linreg (very bare rn)

--- src/blocks/LinearRegression.js
+++ src/blocks/LinearRegression.js
@@ -1,6 +1,5 @@
 const SimpleLinearRegression = require('ml-regression-simple-linear');
 const MLR = require('ml-regression-multivariate-linear');
-const { Matrix } = require('ml-matrix');
 
 class LinReg {
 	loadData(X, y) {
@@ -11,22 +10,36 @@ class LinReg {
 	fit() {
 		if (this.X[0][0] === undefined) {
 			console.log('SLR');
+			this.slr = new SimpleLinearRegression(this.X, this.y);
 		} else {
 			console.log('MLR');
+			if (this.y[0][0] == undefined) {
+				this.mlr = new MLR(this.X, this.y);
+			}
 		}
 	}
 	predict() {}
 }
 
-const model = new LinReg();
+const simpleLR = new LinReg();
 
-model.loadData(
+simpleLR.loadData([1, 2, 3], [1, 2, 3]);
+
+simpleLR.fit();
+
+console.log(simpleLR.slr);
+
+const multiLR = new LinReg();
+
+multiLR.loadData(
 	[
-		[1, 2],
-		[2, 3],
-		[4, 5]
+		[1, 2, 3],
+		[2, 3, 4],
+		[5, 6, 7]
 	],
-	[1, 2, 3]
+	[[1], [2], [3]]
 );
 
-model.fit();
+multiLR.fit();
+
+console.log(multiLR.mlr);
