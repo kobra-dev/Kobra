4b76743ff75bec301497a7744a6e15988c5071b6
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-15 10:12:27 -0700

    predict function works

--- src/blocks/LinearRegression.js
+++ src/blocks/LinearRegression.js
@@ -13,11 +13,9 @@ class LinReg {
 	fit() {
 		//fits linreg to dataset
 		if (this.X[0][0] === undefined) {
-			console.log('SLR');
 			this.slr = new SimpleLinearRegression(this.X, this.y);
 			this.modelType = 'SLR';
 		} else {
-			console.log('MLR');
 			if (this.y[0][0] == undefined) {
 				for (var i = 0; i < this.y.length; i++) {
 					this.y[i] = [this.y[i]];
@@ -27,8 +25,21 @@ class LinReg {
 			this.modelType = 'MLR';
 		}
 	}
-	predict() {
-		//makes a prediction
+	predict(X) {
+		if (this.modelType === 'SLR') {
+			return this.slr.toJSON().slope * X + this.slr.toJSON().intercept;
+		} else {
+			var sum = 0;
+			for (var i = 0; i < X.length; i++) {
+				sum += X[i] * this.mlr.toJSON().weights[i][0];
+				console.log(sum);
+			}
+			const weights = this.mlr.toJSON().weights;
+			sum += Number(weights[weights.length - 1]);
+
+			sum = Math.round((sum * (10 ^ 5)) / (10 ^ 5));
+			return sum;
+		}
 	}
 }
 
@@ -39,7 +50,11 @@ simpleLR.loadData([1, 2, 3], [1, 2, 3]);
 
 simpleLR.fit();
 
-console.log(simpleLR.slr);
+console.log(simpleLR.slr.toJSON());
+
+console.log(simpleLR.modelType);
+
+console.log(simpleLR.predict(3));
 
 const multiLR = new LinReg();
 
@@ -49,11 +64,11 @@ multiLR.loadData(
 		[2, 3, 4],
 		[5, 6, 7]
 	],
-	[1, 2, 3]
+	[7, 10, 19]
 );
 
 multiLR.fit();
 
 console.log(multiLR.mlr.toJSON());
 
-console.log(multiLR.mlr.modelType);
+console.log(multiLR.predict([1, 2, 3]));
