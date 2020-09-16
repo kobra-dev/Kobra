f1cfd17014a4b97ca64c06cf141a6144e4036670
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-15 17:47:26 -0700

    it works :D

--- src/blocks/LogisticRegression.js
+++ src/blocks/LogisticRegression.js
@@ -9,7 +9,7 @@ class LogReg {
 	}
 
 	fit() {
-		this.model = LogisticRegression({ numSteps: 1000, learningRate: 5e-3 });
+		this.model = new LogisticRegression({ numSteps: 1000, learningRate: 5e-3 });
 		this.model.train(this.X, this.y);
 	}
 
@@ -23,34 +23,39 @@ class LogReg {
 	}
 }
 
-const Xd = [
-	[0, -1],
-	[1, 0],
-	[1, 1],
-	[1, -1],
-	[2, 0],
-	[2, 1],
-	[2, -1],
-	[3, 2],
-	[0, 4],
-	[1, 3],
-	[1, 4],
-	[1, 5],
-	[2, 3],
-	[2, 4],
-	[2, 5],
-	[3, 4],
-	[1, 10],
-	[1, 12],
-	[2, 10],
-	[2, 11],
-	[2, 14],
-	[3, 11]
-];
-const Y = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2];
+let LogisticReg = new LogReg();
 
-// The test set (Xtest, Ytest).
-const Xtest = [
+LogisticReg.loadData(
+	[
+		[0, -1],
+		[1, 0],
+		[1, 1],
+		[1, -1],
+		[2, 0],
+		[2, 1],
+		[2, -1],
+		[3, 2],
+		[0, 4],
+		[1, 3],
+		[1, 4],
+		[1, 5],
+		[2, 3],
+		[2, 4],
+		[2, 5],
+		[3, 4],
+		[1, 10],
+		[1, 12],
+		[2, 10],
+		[2, 11],
+		[2, 14],
+		[3, 11]
+	],
+	[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
+);
+
+LogisticReg.fit();
+
+LogisticReg.predict([
 	[0, -2],
 	[1, 0.5],
 	[1.5, -1],
@@ -60,12 +65,4 @@ const Xtest = [
 	[1, 10.5],
 	[2.5, 10.5],
 	[2, 11.5]
-];
-const Ytest = Matrix.columnVector([0, 0, 0, 1, 1, 1, 2, 2, 2]);
-
-// We will train our model.
-const logreg = new LogisticRegression({ numSteps: 1000, learningRate: 5e-3 });
-logreg.train(X, Y);
-
-// We try to predict the test set.
-const finalResults = logreg.predict(Xtest);
+]);
