e7dcd26d76a9144b8682b2ad7e1c555239342a55
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-16 00:14:51 -0700

    load data and fit done before debugging lmao

--- src/blocks/KNN.js
+++ src/blocks/KNN.js
@@ -1,7 +1,13 @@
 const KNN = require('ml-knn');
 
 class KNN {
-	loadData() {}
-	fit() {}
+	loadData(X, y) {
+		this.X = X;
+		this.y = y;
+	}
+	fit(k) {
+		this.k = k;
+		this.model = new KNN(this.X, this.y, { k: this.k });
+	}
 	predict() {}
 }
