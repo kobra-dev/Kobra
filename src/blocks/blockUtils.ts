407e56c316022a841e2181d33d81fe81769c06b1
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-17 09:27:57 -0700

    reformatted blocks files

--- src/blocks/blockUtils.ts
+++ src/blocks/blockUtils.ts
@@ -2,62 +2,77 @@ import Blockly from 'blockly/core';
 import 'blockly/javascript_compressed';
 
 export interface BlocklyJSDef {
-    block : string;
-    f : { (block : Blockly.Block) : void; };
+  block: string;
+  f: { (block: Blockly.Block): void };
 }
 
 export enum ArgType {
-    Value,
-    Variable,
-    Field
+  Value,
+  Variable,
+  Field
 }
 
 export interface TypedArg {
-    type : ArgType;
-    arg : string;
+  type: ArgType;
+  arg: string;
 }
 
-export function constructCodeFromParams(block : Blockly.Block, method : string, ...args : string[] | TypedArg[]) : string {
-    let result = method + "(";
-    args.forEach((arg : string | TypedArg, index : number) => {
-        if(index !== 0) {
-            result += ", ";
+export function constructCodeFromParams(
+  block: Blockly.Block,
+  method: string,
+  ...args: string[] | TypedArg[]
+): string {
+  let result = method + '(';
+  args.forEach((arg: string | TypedArg, index: number) => {
+    if (index !== 0) {
+      result += ', ';
+    }
+    if (typeof arg === 'string') {
+      // @ts-ignore
+      result += Blockly.JavaScript.valueToCode(
+        block,
+        arg,
+        Blockly.JavaScript.ORDER_ATOMIC
+      );
+    } else {
+      let typedArg: TypedArg = arg as TypedArg;
+      switch (typedArg.type) {
+        case ArgType.Value: {
+          // @ts-ignore
+          result += Blockly.JavaScript.valueToCode(
+            block,
+            typedArg.arg,
+            Blockly.JavaScript.ORDER_ATOMIC
+          );
+          break;
         }
-        if(typeof arg === "string") {
-            // @ts-ignore
-            result += Blockly.JavaScript.valueToCode(block, arg, Blockly.JavaScript.ORDER_ATOMIC);
+        case ArgType.Variable: {
+          // @ts-ignore
+          result += Blockly.JavaScript.variableDB_.getName(
+            block.getFieldValue(typedArg.arg),
+            Blockly.Variables.NAME_TYPE
+          );
+          break;
         }
-        else {
-            let typedArg : TypedArg = arg as TypedArg;
-            switch(typedArg.type) {
-                case ArgType.Value: {
-                    // @ts-ignore
-                    result += Blockly.JavaScript.valueToCode(block, typedArg.arg, Blockly.JavaScript.ORDER_ATOMIC);
-                    break;
-                }
-                case ArgType.Variable: {
-                    // @ts-ignore
-                    result += Blockly.JavaScript.variableDB_.getName(block.getFieldValue(typedArg.arg), Blockly.Variables.NAME_TYPE);
-                    break;
-                }
-                case ArgType.Field: {
-                    result += '"' + block.getFieldValue(typedArg.arg).replace('"', '\\"') + '"';
-                    break;
-                }
-            }
+        case ArgType.Field: {
+          result +=
+            '"' + block.getFieldValue(typedArg.arg).replace('"', '\\"') + '"';
+          break;
         }
-    });
-    result += ")"
-    
-    return result;
+      }
+    }
+  });
+  result += ')';
+
+  return result;
 }
 
-export function valuePkg(code : string) : string[] {
-    // @ts-ignore
-    return [code, Blockly.JavaScript.ORDER_NONE];
+export function valuePkg(code: string): string[] {
+  // @ts-ignore
+  return [code, Blockly.JavaScript.ORDER_NONE];
 }
 
 // We don't really need a function for this but it makes things more consistent
-export function statementPkg(code : string) : string {
-    return code + ";"
+export function statementPkg(code: string): string {
+  return code + ';';
 }
