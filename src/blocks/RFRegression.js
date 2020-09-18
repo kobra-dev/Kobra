11bb26bab98efdd0c270bacefd24a33ad0248014
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-17 18:11:49 -0700

    finished loadData

--- src/blocks/RFRegression.js
+++ src/blocks/RFRegression.js
@@ -2,7 +2,18 @@ const RF = require('ml-random-forest');
 const RFRegressor = RF.RandomForestRegression;
 
 class RFRegression {
-    loadData(X,y){}
-    fit()
-    predict()
+  loadData(X, y) {
+    this.X = X;
+    this.y = y;
+  }
+  fit() {
+    this.model = new RFRegressor({
+      seed: 3,
+      maxFeatures: 2,
+      replacement: false,
+      nEstimators: 200
+    });
+    this.model.train(this.X, this.y);
+  }
+  predict() {}
 }
