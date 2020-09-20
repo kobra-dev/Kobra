ee481394ccfd4d6640a4ed74791cb7b56a23f18b
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-19 23:27:14 -0700

    no errors on fit :D

--- src/blocks/SVC.js
+++ src/blocks/SVC.js
@@ -25,20 +25,16 @@ class SVC {
 	}
 }
 
-var svm = new SVM(options);
+const svcTest = new SVC();
 
-// Train the classifier - we give him an xor
-var features = [
-	[0, 0],
-	[0, 1],
-	[1, 1],
-	[1, 0]
-];
-var labels = [1, -1, 1, -1];
-svm.train(features, labels);
+svcTest.loadData(
+	[
+		[0, 0],
+		[0, 1],
+		[1, 1],
+		[1, 0]
+	],
+	[1, -1, 1, -1]
+);
 
-// Let's see how narrow the margin is
-var margins = svm.margin(features);
-
-// Let's see if it is separable by testing on the training data
-console.log(svm.predict(features)); // [1, -1, 1, -1]
+svcTest.fit();
