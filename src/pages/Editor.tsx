0e657997ebc2c095c066da77642958ce63de8934
merlin04 <Merlin04@users.noreply.github.com>
2020-09-27 11:40:59 -0700

    Start DataView

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -3,17 +3,16 @@ import './Editor.css';
 import PageLayout from '../components/PageLayout';
 import CodeEditor from '../components/CodeEditor';
 import Runner from '../components/Runner';
-import { Paper, Button } from '@material-ui/core';
-import { getCode, componentDidMount } from '../components/CodeEditor';
+import DataView from '../components/DataView';
+import { Paper } from '@material-ui/core';
+import { getCode } from '../components/CodeEditor';
 
 export default function Editor(): React.ReactElement {
 	return (
 		<PageLayout title="Kobra Studio">
 			<div className="gridContainer">
 				<div className="toolsColumn">
-					<Paper style={{ gridArea: 'panel1' }}>
-						<Button onClick={componentDidMount}>Set theme</Button>
-					</Paper>
+					<DataView />
 					<Runner getCode={ () => getCode() } />
 				</div>
 				<Paper style={{ gridArea: 'editor' }} className="editorColumn">
