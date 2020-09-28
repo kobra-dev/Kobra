60fea40abf62f7555540305dafb81d38f24fdc30
Pranav Teegavarapu <pranavnt@outlook.com>
2020-09-28 02:04:20 -0700

    reformatted a ton of files lol

--- src/components/DataView.tsx
+++ src/components/DataView.tsx
@@ -4,65 +4,72 @@ import Plot from 'react-plotly.js';
 
 import './DataView.css';
 
-interface IDataViewProps {
-}
+interface IDataViewProps {}
 
 interface IPlotState {
-  isActive : boolean
-  testStr : string
+  isActive: boolean;
+  testStr: string;
 }
 
-let getComponentPlotState : { () : IPlotState };
-let setComponentPlotState : { (newState : IPlotState) : void };
+let getComponentPlotState: { (): IPlotState };
+let setComponentPlotState: { (newState: IPlotState): void };
 
-export function setTest(newVal : string) {
+export function setTest(newVal: string) {
   const newState = getComponentPlotState();
-  newState.testStr = "Hello world!"
+  newState.testStr = 'Hello world!';
   setComponentPlotState(newState);
 }
 
-export default class DataView extends React.Component<IDataViewProps, IPlotState> {
-  constructor(props : IDataViewProps) {
+export default class DataView extends React.Component<
+  IDataViewProps,
+  IPlotState
+> {
+  constructor(props: IDataViewProps) {
     super(props);
     this.state = {
       isActive: false,
-      testStr: ""
-    }
-    
-    getComponentPlotState = () => { return this.state; };
-    setComponentPlotState = newState => {
+      testStr: ''
+    };
+
+    getComponentPlotState = () => {
+      return this.state;
+    };
+    setComponentPlotState = (newState) => {
       this.state = newState;
       this.forceUpdate();
-    }
+    };
   }
   render() {
     return (
       <Paper className="dataViewContainer">
-        { this.state.isActive
-        ? <Plot
-        data={[
-          {
-            type: 'scatter',
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            marker: {color: 'red'}
-          },
-          {
-            type: 'bar',
-            x: [1, 2, 3],
-            y: [2, 5, 3]
-          }
-        ]}
-        layout={{
-          title: 'Hello World',
-          autosize: true,
-        }}
-        useResizeHandler
-        style={{ width: "100%", height: "100%" }}
-      />
-      : <Typography className="placeholderText">Data visualizations will appear here</Typography>
-      }
-      <p>{this.state.testStr}</p>
+        {this.state.isActive ? (
+          <Plot
+            data={[
+              {
+                type: 'scatter',
+                x: [1, 2, 3],
+                y: [2, 6, 3],
+                marker: { color: 'red' }
+              },
+              {
+                type: 'bar',
+                x: [1, 2, 3],
+                y: [2, 5, 3]
+              }
+            ]}
+            layout={{
+              title: 'Hello World',
+              autosize: true
+            }}
+            useResizeHandler
+            style={{ width: '100%', height: '100%' }}
+          />
+        ) : (
+          <Typography className="placeholderText">
+            Data visualizations will appear here
+          </Typography>
+        )}
+        <p>{this.state.testStr}</p>
       </Paper>
     );
   }
--- src/components/Runner.tsx
+++ src/components/Runner.tsx
@@ -8,146 +8,146 @@ import './Runner.css';
 import { runInContext, highlightBlock, RunResult } from './RunnerContext';
 
 interface IRunnerProps {
-	getCode: { (): string };
+  getCode: { (): string };
 }
 
 export default function Runner(props: IRunnerProps) {
-	const [runnerConsole, setRunnerConsole] = useState(null as Console | null);
-	let userInputCallback: { (result: string): void } | undefined;
+  const [runnerConsole, setRunnerConsole] = useState(null as Console | null);
+  let userInputCallback: { (result: string): void } | undefined;
 
-	async function run(): Promise<void> {
-		runnerConsole?.setBusy(true);
-		runnerConsole?.logX(
-			'run-start',
-			'Run started at ' + new Date().toLocaleTimeString()
-		);
+  async function run(): Promise<void> {
+    runnerConsole?.setBusy(true);
+    runnerConsole?.logX(
+      'run-start',
+      'Run started at ' + new Date().toLocaleTimeString()
+    );
 
-		const source: string = props.getCode();
+    const source: string = props.getCode();
 
-		globalThis.runnerConsole = runnerConsole;
-		globalThis.runnerConsoleGetInput = runnerConsoleGetInput;
-		const runResult: RunResult | undefined = await runInContext(source);
-		delete globalThis.runnerConsole;
-		delete globalThis.runnerConsoleGetInput;
+    globalThis.runnerConsole = runnerConsole;
+    globalThis.runnerConsoleGetInput = runnerConsoleGetInput;
+    const runResult: RunResult | undefined = await runInContext(source);
+    delete globalThis.runnerConsole;
+    delete globalThis.runnerConsoleGetInput;
 
-		if (!(runResult === undefined)) {
-			// There was an exception
-			runnerConsole?.logX('exception', 'EXCEPTION');
+    if (!(runResult === undefined)) {
+      // There was an exception
+      runnerConsole?.logX('exception', 'EXCEPTION');
 
-			let blockType: string | undefined = undefined;
-			if (runResult.blockId !== undefined) {
-				// Get block
-				// @ts-ignore
-				const block = Blockly.getMainWorkspace().getBlockById(
-					runResult.blockId
-				);
-				if (block !== null) {
-					blockType = block.type;
-				}
-			}
+      let blockType: string | undefined = undefined;
+      if (runResult.blockId !== undefined) {
+        // Get block
+        // @ts-ignore
+        const block = Blockly.getMainWorkspace().getBlockById(
+          runResult.blockId
+        );
+        if (block !== null) {
+          blockType = block.type;
+        }
+      }
 
-			if (blockType === undefined) {
-				runnerConsole?.logX(
-					'exception-details',
-					'Block type: could not be identified'
-				);
-				runnerConsole?.logX(
-					'exception-details',
-					'This usually means you did not connect a required input to a block'
-				);
-			} else {
-				runnerConsole?.logX('exception-details', 'Block type: ' + blockType);
-			}
+      if (blockType === undefined) {
+        runnerConsole?.logX(
+          'exception-details',
+          'Block type: could not be identified'
+        );
+        runnerConsole?.logX(
+          'exception-details',
+          'This usually means you did not connect a required input to a block'
+        );
+      } else {
+        runnerConsole?.logX('exception-details', 'Block type: ' + blockType);
+      }
 
-			runnerConsole?.logX('exception-details', runResult.exception);
+      runnerConsole?.logX('exception-details', runResult.exception);
 
-			// Now log in browser console
-			console.log('%cException in generated code', 'color: red');
-			console.log(runResult.exception);
-			console.log('Generated code:');
-			console.log(source);
-		}
+      // Now log in browser console
+      console.log('%cException in generated code', 'color: red');
+      console.log(runResult.exception);
+      console.log('Generated code:');
+      console.log(source);
+    }
 
-		highlightBlock('');
-		runnerConsole?.logX(
-			'run-end',
-			'Run ended at ' + new Date().toLocaleTimeString()
-		);
-		runnerConsole?.setBusy(false);
-	}
+    highlightBlock('');
+    runnerConsole?.logX(
+      'run-end',
+      'Run ended at ' + new Date().toLocaleTimeString()
+    );
+    runnerConsole?.setBusy(false);
+  }
 
-	function copyLog(): void {
-		let output = '';
-		runnerConsole?.state.log?.forEach((logEntry) => {
-			if (logEntry.command.length > 0) {
-				output += logEntry.label + logEntry.command + '\n';
-			}
-			logEntry.message.forEach((logMessage) => {
-				output += logMessage.value + '\n';
-			});
-		});
+  function copyLog(): void {
+    let output = '';
+    runnerConsole?.state.log?.forEach((logEntry) => {
+      if (logEntry.command.length > 0) {
+        output += logEntry.label + logEntry.command + '\n';
+      }
+      logEntry.message.forEach((logMessage) => {
+        output += logMessage.value + '\n';
+      });
+    });
 
-		if (!navigator.clipboard) {
-			runnerConsole?.logX(
-				'exception-details',
-				'Your browser does not support the Clipboard API.'
-			);
-			return;
-		}
+    if (!navigator.clipboard) {
+      runnerConsole?.logX(
+        'exception-details',
+        'Your browser does not support the Clipboard API.'
+      );
+      return;
+    }
 
-		navigator.clipboard.writeText(output).then(
-			() => {},
-			(err) => {
-				runnerConsole?.logX('exception-details', 'Error copying text: ' + err);
-			}
-		);
-	}
+    navigator.clipboard.writeText(output).then(
+      () => {},
+      (err) => {
+        runnerConsole?.logX('exception-details', 'Error copying text: ' + err);
+      }
+    );
+  }
 
-	function runnerConsoleGetInput(): Promise<string> {
-		return new Promise((resolve) => {
-			userInputCallback = (result) => {
-				resolve(result);
-				userInputCallback = undefined;
-			};
-		});
-	}
+  function runnerConsoleGetInput(): Promise<string> {
+    return new Promise((resolve) => {
+      userInputCallback = (result) => {
+        resolve(result);
+        userInputCallback = undefined;
+      };
+    });
+  }
 
-	function processUserInput(text: string) {
-		if (!(userInputCallback === undefined)) {
-			userInputCallback(text);
-		} else {
-			runnerConsole?.setBusy(false);
-		}
-	}
+  function processUserInput(text: string) {
+    if (!(userInputCallback === undefined)) {
+      userInputCallback(text);
+    } else {
+      runnerConsole?.setBusy(false);
+    }
+  }
 
-	return (
-		<Paper className="runnerContainer">
-			<div key={'runnercontrols'} className="runnerControls">
-				<Button startIcon={<PlayArrow />} onClick={run}>
-					Run
-				</Button>
-				<div className="right">
-					<Button startIcon={<FileCopy />} onClick={copyLog}>
-						Copy log
-					</Button>
-					<Button
-						startIcon={<Clear />}
-						onClick={() => {
-							runnerConsole?.clearScreen();
-						}}
-					>
-						Clear
-					</Button>
-				</div>
-			</div>
-			<Console
-				key={'runnerconsole'}
-				ref={(ref) => setRunnerConsole(ref)}
-				handler={processUserInput}
-				promptLabel={'> '}
-				welcomeMessage={'The output of your program will be displayed here'}
-				autofocus={false}
-			/>
-		</Paper>
-	);
+  return (
+    <Paper className="runnerContainer">
+      <div key={'runnercontrols'} className="runnerControls">
+        <Button startIcon={<PlayArrow />} onClick={run}>
+          Run
+        </Button>
+        <div className="right">
+          <Button startIcon={<FileCopy />} onClick={copyLog}>
+            Copy log
+          </Button>
+          <Button
+            startIcon={<Clear />}
+            onClick={() => {
+              runnerConsole?.clearScreen();
+            }}
+          >
+            Clear
+          </Button>
+        </div>
+      </div>
+      <Console
+        key={'runnerconsole'}
+        ref={(ref) => setRunnerConsole(ref)}
+        handler={processUserInput}
+        promptLabel={'> '}
+        welcomeMessage={'The output of your program will be displayed here'}
+        autofocus={false}
+      />
+    </Paper>
+  );
 }
