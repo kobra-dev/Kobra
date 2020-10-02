01f813afec71467a73447cd47c89f12f3c25946a
merlin04 <Merlin04@users.noreply.github.com>
2020-10-01 17:56:52 -0700

    Save/load console, save dialog

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -2,37 +2,46 @@ import React, { useState } from 'react';
 import './Editor.css';
 import PageLayout from '../components/PageLayout';
 import CodeEditor, { getXml, loadXml } from '../components/CodeEditor';
-import Runner from '../components/Runner';
+import Runner, { getConsoleState, setConsoleState } from '../components/Runner';
 import DataView, { IPlotState, getState as getPlotState, editState as editPlotState } from '../components/DataView';
 import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button, Paper } from '@material-ui/core';
 import { getCode } from '../components/CodeEditor';
+import { ConsoleState } from 'react-console-component';
 
 interface SaveData {
   blocklyXml : string,
-  plotState : IPlotState
+  plotState : IPlotState,
+  consoleState : ConsoleState
 }
 
 export default function Editor(): React.ReactElement {
-  const [modalOpen, modalSetOpen] = useState(false);
+  const [openModalOpen, openModalSetOpen] = useState(false);
   const [saveInput, setSaveInput] = useState("");
   const [openButtonDisabled, setOpenButtonDisabled] = useState(true);
+  const [saveModalOpen, saveModalSetOpen] = useState(false);
+  const [saveModalCode, setSaveModalCode] = useState("");
 
-  const handleModalClose = () => {
-    modalSetOpen(false);
+  const handleOpenModalClose = () => {
+    openModalSetOpen(false);
+  }
+
+  const handleSaveModalClose = () => {
+    saveModalSetOpen(false);
   }
 
   function save() {
     const sd : SaveData = {
        blocklyXml: getXml(),
-       plotState: getPlotState()
+       plotState: getPlotState(),
+       consoleState: getConsoleState() as ConsoleState
     };
-    // TODO
-    console.log(JSON.stringify(sd));
+    setSaveModalCode(JSON.stringify(sd));
+    saveModalSetOpen(true);
   }
 
   function open() {
     setOpenButtonDisabled(true);
-    modalSetOpen(true);
+    openModalSetOpen(true);
   }
 
   function saveInputOnChange(event : any) {
@@ -49,6 +58,7 @@ export default function Editor(): React.ReactElement {
         state[key] = sd.plotState[key];
       });
     });
+    setConsoleState(sd.consoleState);
   }
 
   return (
@@ -62,19 +72,31 @@ export default function Editor(): React.ReactElement {
           <CodeEditor />
         </Paper>
       </div>
-      <Dialog open={modalOpen} onClose={handleModalClose}>
+      <Dialog open={openModalOpen} onClose={handleOpenModalClose}>
         <DialogTitle>Open</DialogTitle>
         <DialogContent>
           <DialogContentText>Paste project savedata here.</DialogContentText>
           <TextField multiline rows={4} onChange={ saveInputOnChange } />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleModalClose}>
+          <Button onClick={handleOpenModalClose}>
             Cancel
           </Button>
-          <Button onClick={() => {handleModalClose(); loadSave();}} color="primary" disabled={ openButtonDisabled }>
+          <Button onClick={() => {handleOpenModalClose(); loadSave();}} color="primary" disabled={ openButtonDisabled }>
             Open
           </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={saveModalOpen} onClose={handleSaveModalClose}>
+        <DialogTitle>Save</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Copy this project savedata and store it somewhere safe. You can load it again by selecting File-&gt;Open, and pasting the savedata into the text field.</DialogContentText>
+          <div className="saveModalCodeContainer">
+            <code>{saveModalCode}</code>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSaveModalClose}>Close</Button>
         </DialogActions>
       </Dialog>
     </PageLayout>
