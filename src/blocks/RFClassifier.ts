042f89bd051c8e7bdc7b795721485b49edc622d5
merlin04 <Merlin04@users.noreply.github.com>
2020-10-13 19:31:40 -0700

    RFClassifier TypeScript

---
+++ src/blocks/RFClassifier.ts
@@ -0,0 +1,41 @@
+import { RandomForestClassifier } from 'ml-random-forest';
+import { MLModuleConfig } from './MLModel';
+
+export class RFClassification {
+  X : number[] | undefined;
+  y : number[] | undefined;
+  model : RandomForestClassifier | undefined;
+  seed : number | undefined;
+
+  loadData(X : number[], y : number[]) {
+    this.X = X;
+    this.y = y;
+
+    this.seed = X[0].length;
+  }
+
+  fit() {
+    let hyperparams = {
+      seed: this.seed,
+      maxFeatures: 2,
+      replacement: false,
+      nEstimators: 200
+    };
+
+    this.model = new RandomForestClassifier(hyperparams);
+    this.model?.train(this.X, this.y);
+  }
+
+  predict(X : number) {
+    return this.model?.predict([X]);
+  }
+}
+
+export const _MLModuleConfig : MLModuleConfig = {
+  createStr: "random forest classifier model",
+  fitStr: "fit random forest classifier model",
+  predictStr: "predict with random forest classifier model",
+  colour: 150,
+  blockPrefix: "rfc",,
+  additionalFitParams: []
+};
