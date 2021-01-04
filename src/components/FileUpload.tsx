2f515d4f4ee95611a979d05ff2c65d3eae86c5f9
Pranav Teegavarapu <pranavnt@outlook.com>
2021-01-04 13:02:20 -0800

    basic file upload structure

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+
+export default function FileUpload() {
+  const [files, setFiles] = useState([]);
+
+  function handleUpload(uploadedFiles) {}
+
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
