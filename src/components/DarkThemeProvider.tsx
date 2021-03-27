70cfc4c76e67f94e2a2ad6e6dc276afdfdda3f2a
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-27 23:01:01 +0200

    fix

--- src/components/DarkThemeProvider.tsx
+++ src/components/DarkThemeProvider.tsx
@@ -1,56 +1,52 @@
-import React, { useState, createContext, useContext, useEffect, useMemo } from 'react';
-import Cookies from 'js-cookie';
-import getMuiTheme from './getMuiTheme';
-import { ThemeProvider } from '@material-ui/core';
+import React, {
+    useState,
+    createContext,
+    useContext,
+    useEffect,
+    useMemo
+} from "react";
+import Cookies from "js-cookie";
+import getMuiTheme from "./getMuiTheme";
+import { ThemeProvider } from "@material-ui/core";
 
 export const DarkContext = createContext([false, (_: boolean) => {}] as [
-  boolean,
-  { (_: boolean): void }
+    boolean,
+    { (_: boolean): void }
 ]);
 
 interface DarkThemeProviderProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export function DarkThemeProvider(props: DarkThemeProviderProps) {
-  const [isDark, _setDark] = useState(Cookies.get('isDarkTheme') === 'true');
-  const setDark = (themeEnabled: boolean) => {
-    _setDark(themeEnabled);
-    Cookies.set('isDarkTheme', themeEnabled.toString());
-  };
-<<<<<<< HEAD
-
-  return (
-    <DarkContext.Provider value={[isDark, setDark]}>
-      {props.children}
-    </DarkContext.Provider>
-  );
-=======
+    const [isDark, _setDark] = useState(Cookies.get("isDarkTheme") === "true");
+    const setDark = (themeEnabled: boolean) => {
+        _setDark(themeEnabled);
+        Cookies.set("isDarkTheme", themeEnabled.toString());
+    };
 
     useEffect(() => {
-        if(globalThis.window !== undefined) {
-            document.body.style.backgroundColor = isDark ? "#121212" : "#ffffff";
+        if (globalThis.window !== undefined) {
+            document.body.style.backgroundColor = isDark
+                ? "#121212"
+                : "#ffffff";
         }
     }, [isDark]);
 
-    const theme = useMemo(
-        () => getMuiTheme(isDark),
-        [isDark]
-    );
+    const theme = useMemo(() => getMuiTheme(isDark), [isDark]);
 
     return (
         <ThemeProvider theme={theme}>
-            <DarkContext.Provider value={ [isDark, setDark] }>
-                { props.children }
+            <DarkContext.Provider value={[isDark, setDark]}>
+                {props.children}
             </DarkContext.Provider>
         </ThemeProvider>
-    ); 
->>>>>>> 9c88e3f58f29f31860299496d233948a8674dc0b
+    );
 }
 
 export function useDarkTheme() {
-  const [isDark, setDark] = useContext(DarkContext);
-  const toggleDark = () => setDark(!isDark);
+    const [isDark, setDark] = useContext(DarkContext);
+    const toggleDark = () => setDark(!isDark);
 
-  return { isDark, setDark, toggleDark };
+    return { isDark, setDark, toggleDark };
 }
--- src/components/UserStatus.tsx
+++ src/components/UserStatus.tsx
@@ -1,48 +1,19 @@
-<<<<<<< HEAD
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { Button, makeStyles, Typography } from '@material-ui/core';
-import { login, logout, useUser } from '../utils/user';
-import fetch from 'isomorphic-unfetch';
-=======
 import React from "react";
 import { Button, makeStyles, Typography } from "@material-ui/core";
-import { useAuthState } from "react-firebase-hooks/auth";
-import firebase, { useUsername } from '../utils/firebase';
+import { useAuthState } from "@kobra-dev/react-firebase-auth-hooks/auth";
+import firebase, { useUsername } from "../utils/firebase";
 import { useLogin } from "./auth/LoginDialogProvider";
->>>>>>> 9c88e3f58f29f31860299496d233948a8674dc0b
 
 const useStyles = makeStyles((theme) => ({
-  loggedIn: {
-    '& p': {
-      verticalAlign: 'middle',
-      display: 'inline-block'
+    loggedIn: {
+        "& p": {
+            verticalAlign: "middle",
+            display: "inline-block"
+        }
+    },
+    userStatus: {
+        marginLeft: "auto"
     }
-<<<<<<< HEAD
-  },
-  userStatus: {
-    marginLeft: 'auto'
-  }
-}));
-
-export default function UserStatus() {
-  const styles = useStyles();
-  const { user } = useUser();
-
-  return (
-    <div className={styles.userStatus}>
-      {user !== null ? (
-        <div className={styles.loggedIn}>
-          <Typography>Hello, {user.nickname}!</Typography>
-          <Button
-            color="inherit"
-            onClick={() => {
-              logout();
-            }}
-          >
-            Log out
-          </Button>
-=======
 }));
 
 export default function UserStatus() {
@@ -53,24 +24,23 @@ export default function UserStatus() {
 
     return (
         <div className={styles.userStatus}>
-            { user ? (
+            {user ? (
                 <div className={styles.loggedIn}>
                     <Typography>Hello{username && `, ${username}`}!</Typography>
-                    <Button color="inherit" onClick={ () => { firebase.auth().signOut(); } }>Log out</Button>
+                    <Button
+                        color="inherit"
+                        onClick={() => {
+                            firebase.auth().signOut();
+                        }}
+                    >
+                        Log out
+                    </Button>
                 </div>
-            ) : (<Button color="inherit" onClick={login}>Log in</Button>) }
->>>>>>> 9c88e3f58f29f31860299496d233948a8674dc0b
+            ) : (
+                <Button color="inherit" onClick={login}>
+                    Log in
+                </Button>
+            )}
         </div>
-      ) : (
-        <Button
-          color="inherit"
-          onClick={() => {
-            login();
-          }}
-        >
-          Log in
-        </Button>
-      )}
-    </div>
-  );
+    );
 }
