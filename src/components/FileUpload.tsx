9071fc6121a6a1e54b0031335fc521d8af3552f6
Pranav Teegavarapu <pranavnt@outlook.com>
2021-04-03 12:22:33 -0700

    DataFrames stored in map

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,54 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { DataFrame } from "../blocks/DataFrame";
-import Dropzone from 'react-dropzone';
-import { Typography, Divider } from '@material-ui/core';
+import Dropzone from "react-dropzone";
+import { Typography, Divider } from "@material-ui/core";
+import { data } from "autoprefixer";
 
 export default function FileUpload() {
-  const [files, setFiles]: any[] = useState([]);
-  const [fileNames, setFileNames]: any[] = useState([null]);
+    const [datasets, setDatasets] = useState(new Map());
 
-  // let fileNames: any[] = [];
+    return (
+        <Dropzone
+            onDrop={(acceptedFiles: File[]) => {
+                let file = acceptedFiles[0];
 
-  return (
-    <Dropzone
-      onDrop={(acceptedFiles: File[]) => {
-        console.log(acceptedFiles);
-        for (let i = 0; i < acceptedFiles.length; i++) {
-          if (acceptedFiles[i].name.split('.').pop() === 'csv') {
-            setFileNames([
-              ...fileNames,
-              <>
-                <Typography variant="body1">{acceptedFiles[i].name}</Typography>
-                <Divider />
-              </>
-            ]);
+                if (file.name.split(".").pop() === "csv") {
+                    /*                   setFileNames([
+                        ...fileNames,
+                          <>
+                            <Typography variant="body1">
+                                {acceptedFiles[0].name}
+                            </Typography>
+                            <Divider />
+                        </>
+                    ]);*/
 
-            let fileReader = new FileReader();
-            fileReader.onloadend = (()=>{
-              const content: String = fileReader.result;
-              const df = new DataFrame();
-              df.read_csv(content);
-              console.log(df);
-            });
-            fileReader.readAsText(acceptedFiles[0]);
-          } else {
+                    let fileReader = new FileReader();
 
-          }
-          setFiles([...files, acceptedFiles]);
-        }
-      }}
-    >
-      {({ getRootProps, getInputProps }) => (
-        <section>
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <div style={{ padding: '2vh', textAlign: 'center' }}>
-              <Typography variant="h6">Drop your files here!</Typography>
-            </div>
-            <div>{fileNames}</div>
-          </div>
-        </section>
-      )}
-    </Dropzone>
-  );
+                    fileReader.onloadend = () => {
+                        const content: string | ArrayBuffer | null =
+                            fileReader.result;
+
+                        const df = new DataFrame();
+
+                        if (typeof content == "string") df.read_csv(content);
+                        else return;
+
+                        setDatasets(datasets.set(file.name, df));
+
+                        console.log(datasets);
+                    };
+
+                    fileReader.readAsText(file);
+                } else {
+                    // TODO: toast that you can only upload a CSV
+                }
+            }}
+        >
+            {({ getRootProps, getInputProps }) => (
+                <section>
+                    <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <div style={{ padding: "2vh", textAlign: "center" }}>
+                            <Typography variant="h6">
+                                Drop your files here!
+                            </Typography>
+                        </div>
+                        <div></div>
+                    </div>
+                </section>
+            )}
+        </Dropzone>
+    );
 }
