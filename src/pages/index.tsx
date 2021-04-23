19a1e9d8d85ac7a4b8903e25588ab859f70b1be9
merlin04 <Merlin04@users.noreply.github.com>
2021-04-22 19:15:53 -0700

    Add placeholder text to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import Head from "next/head";
 import React from "react";
 import PageLayout from "../components/PageLayout";
@@ -9,10 +10,9 @@ export default function Index() {
                 <title>Kobra Studio</title>
             </Head>
             <PageLayout>
-                <a href="/api/login">Login to the app</a>
-                <a href="/api/logout">Logout to the app</a>
-                Hello world!
-                <h1>This should not have a loading screen.</h1>
+                <Typography variant="h1" color="textPrimary">
+                    This page isn't finished yet
+                </Typography>
             </PageLayout>
         </>
     );
