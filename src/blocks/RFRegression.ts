b6cde8ab8c0e7467746a0cb9296b4548a976e9f4
Pranav Teegavarapu <pranavnt@outlook.com>
2021-05-26 02:09:22 -0700

    fixed loaddata for rfregression

--- src/blocks/RFRegression.ts
+++ src/blocks/RFRegression.ts
@@ -1,43 +1,66 @@
-import { RandomForestRegression} from 'ml-random-forest';
-import { BlockType, IMLModel, MLModuleConfig } from './MLModel';
+import { RandomForestRegression } from "ml-random-forest";
+import {
+    BlockType,
+    IMLModel,
+    MLModuleConfig,
+    oneOrTwoDArray,
+    is1DArray
+} from "./MLModel";
 
 export class RFRegression implements IMLModel {
-  X : number[][] | undefined;
-  y : number[] | undefined;
-  model : RandomForestClassifier | undefined;
-  seed : number | undefined;
+    X: number[][] | undefined;
+    y: number[] | undefined;
+    model: RandomForestClassifier | undefined;
+    seed: number | undefined;
 
-  loadData(X:number[][], y:number[]) {
-    this.X = X;
-    this.y = y;
+    loadData(X: oneOrTwoDArray, y: oneOrTwoDArray) {
+        //loads the data
+        if (is1DArray(X)) {
+            let arr: number[][] = [];
 
-    this.seed = X[0].length;
-  }
+            for (let n of X) {
+                arr.push([n]);
+            }
 
-  fit() {
-    let hyperparams = {
-      seed: this.seed,
-      maxFeatures: 2,
-      replacement: false,
-      nEstimators: 200
-    };
+            this.X = arr;
+        } else {
+            this.X = X;
+        }
 
-    this.model = new RandomForestRegression(hyperparams);
-    this.model?.train(this.X, this.y);
-  }
+        if (is1DArray(y)) {
+            this.y = y;
+            console.log(y.length);
+        } else {
+            this.y = y[0];
+        }
 
-  predict(X : number) {
-    return this.model?.predict([X]);
-  }
+        this.seed = this.X.length;
+    }
+
+    fit() {
+        let hyperparams = {
+            seed: this.seed,
+            maxFeatures: 2,
+            replacement: false,
+            nEstimators: 200
+        };
+
+        this.model = new RandomForestRegression(hyperparams);
+        this.model?.train(this.X, this.y);
+    }
+
+    predict(X: number) {
+        return this.model?.predict([X]);
+    }
 }
 
-export const _MLModuleConfig : MLModuleConfig = {
-  createStr: "random forest regression model",
-  fitStr: "fit random forest regression model",
-  predictStr: "predict with random forest regression model",
-  predictInputType: BlockType.Number,
-  predictOutputType: BlockType.Number,
-  colour: 150,
-  blockPrefix: "rfr",
-  additionalFitParams: []
+export const _MLModuleConfig: MLModuleConfig = {
+    createStr: "random forest regression model",
+    fitStr: "fit random forest regression model",
+    predictStr: "predict with random forest regression model",
+    predictInputType: BlockType.Number,
+    predictOutputType: BlockType.Number,
+    colour: 150,
+    blockPrefix: "rfr",
+    additionalFitParams: []
 };
