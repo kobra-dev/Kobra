9717413ed5f48ba88d5544baf6ec746a54f6fcbd
Pranav Teegavarapu <pranavnt@outlook.com>
2021-06-23 00:45:13 -0700

    fixed SVC

--- src/blocks/SVC.ts
+++ src/blocks/SVC.ts
@@ -1,15 +1,31 @@
-import { BlockType, IMLModel, MLModuleConfig } from "./MLModel";
+import { BlockType, IMLModel, MLModuleConfig, is1DArray } from "./MLModel";
 
 var SVM = require("ml-svm");
 
 export class SVC implements IMLModel {
-    X: number[] | undefined;
-    y: number[] | undefined;
+    X: any | undefined;
+    y: any | undefined;
     model: SVM | undefined;
 
-    loadData(X: number[], y: number[]) {
-        this.X = X;
-        this.y = y;
+    loadData(X: any, y: any) {
+        //loads the data
+        if (is1DArray(X)) {
+            this.X = [];
+
+            for (let el of X) {
+                //@ts-ignore
+                this.X.push([el]);
+            }
+        } else {
+            this.X = X[0].map((_, colIndex) => X.map((row) => row[colIndex]));
+        }
+
+        if (is1DArray(y)) {
+            this.y = y;
+        } else {
+            this.y = y[0];
+        }
+
         this.model = new SVM({
             C: 0.01,
             tol: 10e-4,
@@ -23,18 +39,35 @@ export class SVC implements IMLModel {
     }
 
     fit() {
-        if (this.model === undefined) {
-            return;
-        } else {
-            this.model.train(this.X, this.y);
-        }
+        this.model.train(this.X, this.y);
     }
 
-    predict(X: number[] | number[][]): number {
-        if (this.model === undefined) {
-            return -1;
+    predict(X: number | number[] | number[][]) {
+        if (typeof X == "number") {
+            X = [[X]];
+        } else if (X[0][0] === undefined) {
+            if ((this.X as number[][])[0].length === X.length) {
+                X = [X as number[]];
+            } else {
+                let xArr = [];
+
+                for (let el of X) {
+                    xArr.push([el]);
+                }
+
+                X = xArr;
+            }
+        } else {
+            if (X.length === 1 && (X as number[][])[0].length === 1) {
+                let xArr = [];
+
+                for (let el of (X as number[][])[0]) {
+                    xArr.push([el]);
+                }
+            }
         }
-        return this.model.predict([X]);
+
+        return this.model.predict(X);
     }
 }
 
