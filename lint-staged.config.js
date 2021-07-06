6fc67089b34c1594924992209ba5abf64b6ad366
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-07-06 16:11:21 +0200

    feat(ci): setup tests for clone

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'npm run type-check',
-  // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(tsx|jsx|ts)?(x)': (filenames) => `yarn lint . ${filenames.join(' ')}`,
-}
