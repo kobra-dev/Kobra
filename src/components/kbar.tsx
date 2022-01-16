7a07831a3c2e37da509cb669afafbf2baa97215e
Pranav Teegavarapu <pranavnt@outlook.com>
2022-01-16 00:40:55 -0800

    kbar formatting and actions

--- src/components/kbar.tsx
+++ src/components/kbar.tsx
@@ -1,3 +1,4 @@
+import { BorderBottom, BorderColor, Padding } from "@material-ui/icons";
 import {
     KBarProvider,
     KBarPortal,
@@ -19,8 +20,12 @@ export default function KBar({ children }) {
                                 backgroundColor: "white",
                                 borderTopLeftRadius: "0.5rem",
                                 borderTopRightRadius: "0.5rem",
-                                width: "30vw",
-                                fontSize: "1.5rem"
+                                width: "50vw",
+                                maxWidth: "700px",
+                                fontSize: "1.5rem",
+                                border: "2px solid #ddd",
+                                outline: "none",
+                                padding: "0.5rem"
                             }}
                         />
                         <RenderResults />
@@ -41,7 +46,11 @@ function RenderResults() {
             onRender={({ item, active }) => (
                 <div
                     style={{
-                        background: active ? "#eee" : "transparent"
+                        background: active ? "#eee" : "#fff",
+                        paddingLeft: "0.5rem",
+                        border: "1px solid #eee",
+                        borderLeft: "2px #ddd solid",
+                        borderRight: "2px #ddd solid"
                     }}
                 >
                     <>
@@ -74,7 +83,9 @@ function RenderResults() {
                     </>
                     <span
                         style={{
-                            float: "right"
+                            position: "absolute",
+                            top: "0.5rem",
+                            right: "0rem"
                         }}
                     >
                         {
@@ -83,12 +94,14 @@ function RenderResults() {
                                 <span
                                     key={key}
                                     style={{
-                                        fontSize: "1.5rem",
+                                        fontSize: "1.2rem",
                                         backgroundColor: "white",
                                         padding: "0.2rem",
                                         borderRadius: "0.8rem",
                                         alignItems: "center",
-                                        color: "gray"
+                                        marginRight: "0.3rem",
+                                        color: "gray",
+                                        border: "2px #eee solid"
                                     }}
                                 >
                                     {shortcut}
@@ -104,17 +117,46 @@ function RenderResults() {
 
 const actions = [
     {
-        id: "blog",
-        name: "Blog",
-        shortcut: ["b"],
-        keywords: "writing words",
-        perform: () => (window.location.pathname = "blog")
+        id: "new-project",
+        name: "Create New Project",
+        shortcut: ["g", "n"],
+        keywords: "Create new project in Kobra Studio",
+        perform: () => (window.location.pathname = "editor")
     },
     {
-        id: "contact",
-        name: "Contact",
-        shortcut: ["c"],
-        keywords: "email",
-        perform: () => (window.location.pathname = "contact")
+        id: "dashboard",
+        name: "Dashboard",
+        shortcut: ["g", "h"],
+        keywords: "Go to your dashboard",
+        perform: () => (window.location.pathname = "")
+    },
+    {
+        id: "explore",
+        name: "Explore",
+        shortcut: ["g", "e"],
+        keywords: "Explore Kobra projects made by other users :)",
+        perform: () => (window.location.pathname = "explore")
+    },
+    {
+        id: "docs",
+        name: "Documentation",
+        shortcut: ["g", "d"],
+        keywords: "Go to Kobra's documentation",
+        perform: () => (window.location.href = "https://docs.kobra.dev/")
+    },
+    {
+        id: "blog",
+        name: "Blog",
+        shortcut: ["g", "b"],
+        keywords: "Go to Kobra's blog",
+        perform: () => (window.location.href = "https://blog.kobra.dev/")
+    },
+    {
+        id: "github",
+        name: "GitHub",
+        shortcut: ["g", "h"],
+        keywords: "Go to Kobra's GitHub",
+        perform: () =>
+            (window.location.href = "https://github.com/kobra-dev/kobra")
     }
 ];
